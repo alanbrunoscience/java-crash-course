00a06382695925492beca40d557c758524ca51ee
alanbrunoscience <alanbrunomath@gmail.com>
2025-06-07 21:58:30 -0300

    :sparkles: Include the exercise solution of Section 5

---
+++ Sections/Section 5 - Conditional Structure/Projects/src/b/logicalexpressions/Example3_NotOperator.java
@@ -0,0 +1,29 @@
+/**
+ * NOT Operator (!)
+ * 
+ * The NOT operator (!) inverts the condition.
+ * 
+ * !F => V
+ * !V => F
+ * 
+ */
+
+package b.logicalexpressions;
+
+public class Example3_NotOperator {
+
+	public static void main(String[] args) {
+
+		// Variable declaration
+		int x = 5;
+
+		// Data output
+		System.out.println("*** Logical Expressions Examples (!) ***\n");
+		System.out.printf("-> x = %d%n%n", x);
+		System.out.printf("1) !(x == 10) -> %s;%n", !(x == 10));
+		System.out.printf("2) !(x >= 2) -> %s;%n", !(x >= 2));
+		System.out.printf("3) !(x <= 20 && x == 10) -> %s.%n", !(x <= 20 && x == 10));
+
+	}
+
+}
