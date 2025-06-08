b5e7c24c12a9ce9fe2c2df8c0948539b0c881b4b
alanbrunoscience <alanbrunomath@gmail.com>
2025-06-07 21:41:22 -0300

    :sparkles: Include the exercise solution of Section 5

---
+++ Sections/Section 5 - Conditional Structure/Projects/src/a/comparativeexpressions/ComparativeExpressions.java
@@ -0,0 +1,20 @@
+package a.comparativeexpressions;
+
+public class ComparativeExpressions {
+
+	public static void main(String[] args) {
+		
+		// Variable declaration
+		int x = 5;
+		
+		// Data output
+		System.out.println("*** Comparative Expressions ***\n");
+		System.out.printf("-> x = %d%n%n", x);
+		System.out.printf("1) x > 0 -> %s;%n", x > 0);
+		System.out.printf("2) x == 3 -> %s;%n", x == 3);
+		System.out.printf("3) 10 <= 30 -> %s;%n", 10 <= 30);
+		System.out.printf("4) x != 2 -> %s.%n", x != 2);
+
+	}
+
+}
---
+++ Sections/Section 5 - Conditional Structure/Projects/src/b/logicalexpressions/Example2_OrOperator.java
@@ -0,0 +1,31 @@
+/**
+ * OR Operator (||)
+ * 
+ * At least one of the conditions must be true.
+ * 
+ * F || F => F
+ * F || V => V
+ * V || F => V
+ * V || V => V
+ * 
+ */
+
+package b.logicalexpressions;
+
+public class Example2_OrOperator {
+
+	public static void main(String[] args) {
+
+		// Variable declaration
+		int x = 5;
+
+		// Data output
+		System.out.println("*** Logical Expressions Examples (||) ***\n");
+		System.out.printf("-> x = %d%n%n", x);
+		System.out.printf("1) x == 10 || x <= 20 -> %s;%n", x == 10 || x <= 20);
+		System.out.printf("2) x > 0 || x != 3 -> %s;%n", x > 0 || x != 3);
+		System.out.printf("3) x <= 0 || x != 3 || x != 5 -> %s.%n", x <= 0 || x != 3 || x != 5);
+
+	}
+
+}
